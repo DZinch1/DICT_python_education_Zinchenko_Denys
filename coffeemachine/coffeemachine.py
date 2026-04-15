6864db40ef3670bb88630590b7f690df59cbed10
Denys Zinchenko <d.zinchenko@student.khai.edu>
2026-04-15 19:08:42 +0300

    coffeemachine 2-nd stage

---
+++ coffeemachine/coffeemachine.py
@@ -0,0 +1,9 @@
+print("How many cups you will need?")
+cups = int(input('>'))
+water = cups * 200
+milk = cups * 50
+beans = cups * 15
+print(f'For {cups} cups of coffee you will need:')
+print(f'{water} ml of water')
+print(f'{milk} ml of milk')
+print(f'{beans} g of coffee beans')
