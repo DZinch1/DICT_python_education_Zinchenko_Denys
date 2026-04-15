e96cfff498c0aaf902db01e868e200aba2835c8b
Denys Zinchenko <d.zinchenko@student.khai.edu>
2026-04-15 19:50:27 +0300

    coffeemacine 3-rd stage

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,9 +1,27 @@
-print("How many cups you will need?")
-cups = int(input('>'))
-water = cups * 200
-milk = cups * 50
-beans = cups * 15
-print(f'For {cups} cups of coffee you will need:')
-print(f'{water} ml of water')
-print(f'{milk} ml of milk')
-print(f'{beans} g of coffee beans')
+waterforcup = 200
+milkforcup = 50
+beansforcup = 15
+
+water = int(input("Write how many ml of water coffee machine has?\n>"))
+milk = int(input("Write how many ml of milk coffee machine has?\n>"))
+beans = int(input("Write how many grams of coffee beans coffee machine has?\n>"))
+cups = int(input("Write how many cups yiu will need?\n>"))
+
+possiblecupswater = water // waterforcup
+possiblecupsmilk = milk // milkforcup
+possiblecupsbeans = beans // beansforcup
+
+if possiblecupswater <= possiblecupsmilk and possiblecupswater <= possiblecupsbeans:
+    possiblecups = possiblecupswater
+elif possiblecupsmilk <= possiblecupswater and possiblecupsmilk <= possiblecupsbeans:
+    possiblecups = possiblecupsmilk
+else:
+    possiblecups = possiblecupsbeans
+
+
+if possiblecups == cups:
+    print("Yes, I can make that amount of coffee")
+elif possiblecups > cups:
+    print("Yes, I can make that amount of coffee (and even " + str(possiblecups - cups) + " more than that)")
+else:
+    print("No, I can make only " + str(possiblecups) + " cups of coffee")
